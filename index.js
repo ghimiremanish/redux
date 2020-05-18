8d8e93feceeebd96ed96c47c9a24c792b8fe6332
Manish Ghimire <algorithm@Algorithm-2.local>
2020-05-18 23:55:33 +0545

    reducer and actions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hi')
+const redux 
+
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action',
+    }
+}
+
+
+//reducer
+//(previousState, action) => newState
+
+const initialState = {
+    numberOfCakes: 10,
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state,
+            numberOfCakes: state.numberOfCakes - 1
+        }
+
+        default: return state
+
+    }
+}
