61a7daea264d5dc429a53bfcc10fcedd3f325b5f
Manish Ghimire <algorithm@Algorithm-2.local>
2020-05-19 01:24:53 +0545

    async application using redux thunk

--- asyncAction.js
+++ asyncAction.js
@@ -1,5 +1,9 @@
 const redux = require('redux')
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
+
 const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
 
 const initialState = {
     loading: false,
@@ -12,6 +16,7 @@ const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
 const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
 const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
 
+//action creators
 const fetchUserRequest = () => {
     return {
         type: FETCH_USERS_REQUEST,
@@ -43,15 +48,36 @@ const reducer = (state=initialState, action) => {
         case FETCH_USERS_SUCCESS:
             return{
                 ...state,
-                users: action.payload
+                users: action.payload,
+                loading: false
             }
         case FETCH_USERS_FAILURE:
             return{
                 ...state,
+                loading: false,
                 error: action.payload
             }
     }
 }
 
-//store
-const store = createStore(reducer)
+//calling a api (action creator)
+const fetchUsers = () => {
+    return function(dispatch){
+        dispatch(fetchUserRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+            const users = res.data.map(user =>user.name)
+            dispatch(fetchUserSuccess(users))
+        }).catch(err => {
+            // err.message
+            const error = err.message
+            dispatch(fetchUserFailure(error))
+        })
+    }
+}
+
+//creating store with middleware thunk
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+
+//subscribe to the store
+store.subscribe(()=> {console.log(store.getState())})
+store.dispatch(fetchUsers())
